eff55c26b53ec2806cc80613ae97cbc4579ec26d
unknown <cholito9313@gmail.com>
2022-05-18 19:20:19 +0300

    changes

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ function changeTheme() {
     let page = document.getElementById("page");
 
     if (document.getElementById("themes").value == "black") {
-
-        page.classList.toggle("dark");
+        page.classList.remove();
+        page.classList.add("dark");
 
     } else if (document.getElementById("themes").value == "white") {
-
-        page.classList.toggle("white");
+        page.classList.remove();
+        page.classList.add("white");
 
     } else if (document.getElementById("themes").value == "color") {
-        
-        page.classList.toggle("color");
+        page.classList.remove();
+        page.classList.add("color");
     }
 }
