ce2fc52e87fc1c864adbb20220cc2b2e19245f21
unknown <cholito9313@gmail.com>
2022-05-22 20:30:18 +0300

    classlist removed

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ function changeTheme() {
     let page = document.getElementById("page");
 
     if (document.getElementById("themes").value == "black") {
-        page.classList.remove();
-        page.classList.add("dark");
+        document.body.style.background = "rgba(0, 0 ,0)";
+        document.body.style.color = "rgba(250, 250,250)";
 
     } else if (document.getElementById("themes").value == "white") {
-        page.classList.remove();
-        page.classList.add("white");
+        document.body.style.background = "rgba(250, 250,250)";
+        document.body.style.color = "rgba(0, 0,0)";
+
 
     } else if (document.getElementById("themes").value == "color") {
-        page.classList.remove();
-        page.classList.add("color");
+        document.body.style.background = "rgb(223, 241, 175)";
+        document.body.style.color = " rgb(227, 25, 119)";
     }
 }
